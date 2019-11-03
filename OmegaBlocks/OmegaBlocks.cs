1fc084c9b5ca513d2c78b98d42ebaa9825e9106d
OmegaRogue <thatomegarogue@gmail.com>
2019-11-03 17:08:53 +0100

    Start OmegaBlocks

--- OmegaBlocks/OmegaBlocks.cs
+++ OmegaBlocks/OmegaBlocks.cs
@@ -1,6 +1,36 @@
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
 namespace OmegaBlocks
 {
-	public class OmegaBlocks
+	[BepInPlugin("OmegaBlocks", "OmegaBlocks", "1.0.0")]
+	public class OmegaBlocks : BaseUnityPlugin
 	{
+		public OmegaBlocks()
+		{
+			Config = base.Config;
+			Logger = base.Logger;
+
+			OnEnable();
+		}
+
+		public new static ManualLogSource Logger { get; set; }
+		public new static ConfigFile      Config { get; set; }
+
+
+		public void OnLoad()
+		{
+		}
+
+		public void OnEnable()
+		{
+			// += your hooks
+		}
+
+		public void OnDisable()
+		{
+			// -= your hooks
+		}
 	}
 }
